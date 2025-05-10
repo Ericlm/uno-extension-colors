32cd5070846e9b0dd17ac5c4e77ddde2860a5073
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-05-10 22:14:40 +0200

    🐛 Colors in oklch

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo eveniet eligendi sunt ad ratione
-  distinctio reiciendis beatae, voluptatum doloribus, repudiandae eaque, nostrum libero cupiditate
-  explicabo corrupti minima asperiores! Fuga deserunt nemo molestias iure voluptatem pariatur maxime
-  consequatur suscipit, perspiciatis, praesentium laudantium at inventore quis, quasi odit hic rerum
-  fugit labore. Error quasi, aperiam temporibus cupiditate iste reiciendis expedita accusamus nobis,
-  provident incidunt laboriosam quisquam laudantium qui consequatur, dolore totam doloribus esse
-  velit ipsam officia exercitationem debitis ab natus. Laboriosam rem neque reiciendis, incidunt
-  officia ullam asperiores voluptas dolorum. Corporis vitae voluptatibus suscipit? Placeat cumque
-  architecto quam vitae quia, nam iusto.
+  <div class="bg-pink text-white outline-blue outline-1 ring-1 ring-yellow-500">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, sequi.
+  </div>
 </template>
